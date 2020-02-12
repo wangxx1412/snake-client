18cac8d3e5e3afbb6b0c316c58a309f157af16d6
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-02-12 13:53:39 -0800

    Add ability to stop game through ctr+c

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const { connect } = require("./client");
+
 console.log("Connecting ...");
-connect();
+const conn = connect();
+
+const handleUserInput = function(stdin, conn) {
+  stdin.on("data", data => {
+    if (data === "\u0003") {
+      conn.end();
+      process.exit();
+    }
+  });
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  handleUserInput(stdin, conn);
+  return stdin;
+};
+
+setupInput();
