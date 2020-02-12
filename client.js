06b9a231f4bc2eb7155d68b5d1ea98043735dafd
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-02-12 13:32:30 -0800

    Add send msg to client

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+const connect = function() {
+  const conn = net.createConnection({
+    host: "192.168.88.177",
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", data => {
+    console.log("Connected To the Snake Server!!");
+    conn.write("Name: 007");
+  });
+  conn.on("data", data => {
+    console.log("Server says: ", data);
+  });
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,17 +1,3 @@
-const net = require("net");
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "192.168.88.177",
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", data => {
-    console.log("Server says: ", data);
-  });
-  return conn;
-};
-
+const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
